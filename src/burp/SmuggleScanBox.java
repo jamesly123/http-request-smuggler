7f25cebb2df90450bc5faa7f1eb5af4c804bb7f6
JamesPoppyCock <jamesly123@gmail.com>
2020-03-06 09:35:56 +0800

    Fuzz all the methods
    Got the idea, but not completed

--- src/burp/SmuggleScanBox.java
+++ src/burp/SmuggleScanBox.java
@@ -63,9 +63,13 @@ public abstract class SmuggleScanBox extends Scan {
         }
 
         //Fuzz all methods - Added code
-        if (Utilities.globalSettings.getBoolean("FUZZ ALL ZE METHODS")){ //if true (Box is checked)
+        String[] httpverbs = new String[] {"GET","POST","OPTIONS","HEAD","PUT","TRACE","CONNECT","PROPFIND","PROPPATCH","MKCOL","COPY","MOVE","LOCK","UNLOCK","VERSION-CONTROL","REPORT","CHECKOUT","CHECKIN","UNCHECKOUT","MKWORKSPACE","UPDATE","LABEL","MERGE","BASELINE-CONTROL","MKACTIVITY","ORDERPATCH","ACL","PATCH","SEARCH"};
 
-            baseReq = Utilities.setMethod(baseReq, "HTTPVERBHERE");
+        if (Utilities.globalSettings.getBoolean("FUZZ ALL ZE METHODS")){ //if true (Box is checked)
+            for(int i=0; i<httpverbs.length; i++){
+                baseReq = Utilities.setMethod(baseReq, httpverbs[i]);
+            }
+                
         }
         
 
