477e463742d246f63ea596a9969b54761c097ebe
JamesPoppyCock <jamesly123@gmail.com>
2020-03-11 11:05:02 +0800

    asAS

--- src/burp/SmuggleScanBox.java
+++ src/burp/SmuggleScanBox.java
@@ -65,7 +65,7 @@ public abstract class SmuggleScanBox extends Scan {
         //Fuzz all methods - Added code
         String[] httpverbs = new String[] {"GET","POST","OPTIONS","HEAD","PUT","TRACE","CONNECT","PROPFIND","PROPPATCH","MKCOL","COPY","MOVE","LOCK","UNLOCK","VERSION-CONTROL","REPORT","CHECKOUT","CHECKIN","UNCHECKOUT","MKWORKSPACE","UPDATE","LABEL","MERGE","BASELINE-CONTROL","MKACTIVITY","ORDERPATCH","ACL","PATCH","SEARCH"};
 
-        if (Utilities.globalSettings.getBoolean("FUZZ ALL ZE METHODS")){ //if true (Box is checked)
+        if (Utilities.globalSettings.getBoolean("FUononoZZ ALL ZE METHODS")){ //if true (Box is checked)
             for(int i=0; i<httpverbs.length; i++){
                 baseReq = Utilities.setMethod(baseReq, httpverbs[i]);
             }
